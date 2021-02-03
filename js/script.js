06fb9c73bc9db5e5c01d36803351694f71d41143
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-02-03 18:39:49 +0600

    Countdown Added

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const birthday = "2 Jan 2022";
+
+function countdown() {
+    const birthdayDate = new Date(birthday);
+    const currentDate = new Date();
+
+    const totalSeconds = (birthdayDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / (3600 * 24));
+    const hours = Math.floor(totalSeconds % (3600 * 24) / 3600);
+    const minutes = Math.floor(totalSeconds % 3600 / 60);
+    const seconds = Math.floor(totalSeconds % 60);
+
+    document.getElementById("days").innerText = formatTime(days);
+    document.getElementById("hours").innerText = formatTime(hours);
+    document.getElementById("minutes").innerText = formatTime(minutes);
+    document.getElementById("seconds").innerText = formatTime(seconds);
+
+}
+
+function formatTime(time) {
+    return time < 10 ? (`0${time}`) : time;
+}
+
+countdown();
+setInterval(countdown, 1000);
